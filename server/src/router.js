defeccd1f75994b4dc32b171ba9e2134bc7862a8
Nevid Rahal <nnevid@gmail.com>
2023-05-05 14:03:57 +0200

    route update created

--- server/src/router.js
+++ server/src/router.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const loginRoute = require('./routes/loginRoute');
 const readTodosRoute = require('./routes/readTodosRoute');
-const createTodosRoute = require('./routes/createTodosRoute');
+const createTodoRoute = require('./routes/createTodoRoute');
+const updateTodoRoute = require('./routes/updateTodoRoute');
 
 const isLoggedIn = require('./middleware/isLoggedIn');
-
-
 router.post('/login', loginRoute);
+
+//CRUD ROUTES
 router.get('/todos', isLoggedIn, readTodosRoute);
-router.post('/todos', isLoggedIn, createTodosRoute);
+router.post('/todos', isLoggedIn, createTodoRoute);
+router.put('/todos', isLoggedIn, updateTodoRoute);
 
 module.exports = router;
